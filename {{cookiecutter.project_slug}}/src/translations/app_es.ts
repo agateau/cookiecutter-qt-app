7f695828744005b6d5282bbf0b6dec521c4044de
Aurelien Gateau <mail@agateau.com>
2020-05-01 12:05:01 +0200

    Improve translations support

--- {{cookiecutter.project_slug}}/src/translations/app_es.ts
+++ {{cookiecutter.project_slug}}/src/translations/app_es.ts
@@ -1,17 +1,20 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR" sourcelanguage="en_US">
+<TS version="2.1" language="es_ES">
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../MainWindow.ui" line="28"/>
         <source>&amp;File</source>
-        <translation>&amp;Fichier</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../MainWindow.ui" line="38"/>
         <source>Settings</source>
-        <translation type="unfinished">Configuration</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../MainWindow.ui" line="43"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
@@ -19,14 +22,17 @@
 <context>
     <name>SettingsDialog</name>
     <message>
+        <location filename="../SettingsDialog.ui" line="14"/>
         <source>Settings</source>
-        <translation>Configuration</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../SettingsDialog.ui" line="24"/>
         <source>General</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../SettingsDialog.ui" line="30"/>
         <source>More Settings</source>
         <translation type="unfinished"></translation>
     </message>
