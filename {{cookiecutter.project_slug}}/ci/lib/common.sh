8818fe219af9a59b812f452c95bfdcefbfbc2e3b
Aurelien Gateau <mail@agateau.com>
2020-05-24 20:13:50 +0200

    Install icoutils on Windows

--- {{cookiecutter.project_slug}}/ci/lib/common.sh
+++ {{cookiecutter.project_slug}}/ci/lib/common.sh
@@ -46,6 +46,14 @@ die() {
     exit 1
 }
 
+mkabsdir() {
+    mkdir -p "$1"
+    pushd $PWD > /dev/null
+    cd "$1"
+    echo $PWD
+    pop > /dev/null
+}
+
 init_python_cmd() {
     echo_title "Looking for a Python 3 + pip installation"
     for interpreter in python3 python ; do
@@ -72,6 +80,35 @@ init_run_as_root() {
     else
         RUN_AS_ROOT="su -c"
     fi
+}
+
+install_prebuilt_archive() {
+    local url=$1
+    local sha1=$2
+    local download_file=$3
+    local unpack_dir=$4
+
+    echo "Downloading '$url'"
+    curl --location --continue-at - --output "$download_file" "$url"
+
+    echo "Checking integrity"
+    echo "$sha1 $download_file" | sha1sum --check
+
+    echo "Unpacking"
+    (
+        cd "$unpack_dir"
+        case "$download_file" in
+            *.zip)
+                unzip -q "$download_file"
+                ;;
+            *.tar.gz|*.tar.bz2|*.tar.xz)
+                tar xf "$download_file"
+                ;;
+            *)
+                die "Don't know how to unpack $download_file"
+                ;;
+        esac
+    )
 }
 
 detect_os
