7e9e17853abf0438e7f974469c712adccde737ea
Aurelien Gateau <mail@agateau.com>
2020-06-25 09:04:34 +0200

    Add an English "translation"

---
+++ {{cookiecutter.project_slug}}/src/translations/{{cookiecutter.project_slug}}_en.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.ui" line="28"/>
+        <source>&amp;File</source>
+        <translation>&amp;File</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="38"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Settings</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="43"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quit</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../SettingsDialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="24"/>
+        <source>General</source>
+        <translation>General</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="30"/>
+        <source>More Settings</source>
+        <translation>More Settings</translation>
+    </message>
+</context>
+</TS>
